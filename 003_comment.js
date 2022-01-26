6f3a698aa839c9e95754da9c70a86d87c9bc443d
Adarsh Verma <69429900+Adarshv05@users.noreply.github.com>
2022-01-26 23:02:43 +0530

    Add files via upload

---
+++ 003_comment.js
@@ -0,0 +1,9 @@
+console.log("Comment JavaScript")
+
+// in-line comment
+
+/* multiline comment
+yo 
+bois
+whatsupp
+*/
---
+++ 004_variables.js
@@ -0,0 +1,8 @@
+let num = 7
+console.log(num)
+
+num = 9
+console.log(num)
+
+let UserName="John"
+console.log(UserName)
