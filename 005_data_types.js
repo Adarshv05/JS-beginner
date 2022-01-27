9224fdd21afadec3f14fd7b721b6a6d95956e7ab
Adarsh Verma <69429900+Adarshv05@users.noreply.github.com>
2022-01-27 22:25:48 +0530

    Add files via upload

---
+++ 005_data_types.js
@@ -0,0 +1,28 @@
+let name1 = 'John'; //string
+console.log(name1);
+
+let num1 = 6;
+console.log(num1); //number
+let num2 = 7.78;
+console.log(num2);
+
+let num3 = 5/0;
+console.log(num3); // Infinity
+
+let num4 = -6/0;
+console.log(num4); // -Infinity
+
+let num5 = "dfgbdj"/7; 
+console.log(num5);  // string / number = X
+
+let Pass = true;  // true 1
+console.log(Pass);
+
+let Fail = false; // false 0
+console.log(Fail);
+
+let name2 = undefined;
+console.log(name2); // undefined
+
+let num6 = null; //null
+console.log(num6);
