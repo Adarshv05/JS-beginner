b093bdbc576b83d763cd11a662cbb34ec8676361
Adarsh Verma <69429900+Adarshv05@users.noreply.github.com>
2022-01-28 22:44:47 +0530

    Add files via upload

---
+++ 006_data_types_2.js
@@ -0,0 +1,24 @@
+// Reference Data Type
+
+//Object
+var marks =
+{
+    John: 87,
+    Kate: 67,
+    Niti: 88.55 
+}
+
+console.log(marks);
+
+//Array
+
+var arr=[1,2,3,4,5,6,7,8,9]
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[4]);
+
+var arr1=[1,2,"Boiss",3,4]
+
+console.log(arr1)
+console.log(arr1[2])
---
+++ 007_operators.js
@@ -0,0 +1,47 @@
+var a = 95;
+var b = 76;
+
+console.log("Artihmetic Operator")//Artihmetic Operator
+console.log("The value of a+b is",a+b)
+console.log("The value of a-b is",a-b)
+console.log("The value of a*b is",a*b)
+console.log("The value of a/b is",a/b)
+
+console.log("Assignment Operator")//Assignment Operator
+var c = b; 
+console.log(c)
+
+c += 3 // c = c + 3
+console.log(c)
+c -= 3 // c = c - 3
+console.log(c)
+c *= 3 // c = c * 3
+console.log(c)
+c /= 3 // c = c / 3 
+console.log(c)
+
+console.log("Comparision Operator")//Comparision Operator
+var x = 55;
+var y = 75;
+
+console.log(x==y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x>y);
+console.log(x<y);
+
+console.log("Logical Operator")//Logical Operator
+
+console.log(true && true) //and
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+console.log(true || true) //or
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+console.log(!true) //not
+console.log(!false)
+
