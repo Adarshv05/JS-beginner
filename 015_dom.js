21fbb929486f75adfb246287bf60f05b54953b14
Adarsh Verma <69429900+Adarshv05@users.noreply.github.com>
2022-02-02 22:22:07 +0530

    Add files via upload

---
+++ 015_dom.js
@@ -0,0 +1,8 @@
+//DOM Manipulation (Document Object Model)
+
+let elem = document.getElementById("Click")
+console.log(elem)
+
+let elemclass = document.getElementsByClassName("Container")
+console.log(elemclass)
+
